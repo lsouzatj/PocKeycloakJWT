64640cb71882d99a21ce052dbfe0320989ba71af
lsouzatj <lsouza.tj@gmail.com>
2022-09-23 07:21:42 -0300

    Criacao do projeto

---
+++ src/main/java/com/br/keycloak/controller/SecurityResource.java
@@ -0,0 +1,41 @@
+package com.br.keycloak.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping(value = "/security")
+public class SecurityResource {
+
+    /**
+     * endpoint sem nenhuma validação de role
+     */
+    @GetMapping
+    public ResponseEntity<Void> isAuthenticated() {
+        log.warn("GET SecurityResource isAuthenticated: Usuario autenticado.");
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * endpoint onde o usuario tem que ter a role user
+     */
+    @GetMapping(value = "/has-role")
+    public ResponseEntity<Void> isUser() {
+        log.warn("GET SecurityResource isUser: isUser autenticado.");
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * endpoint onde o usuario tem que ter a role admin
+     */
+    @GetMapping(value = "/is-admin")
+    public ResponseEntity<Void> isAdmin() {
+        log.warn("GET SecurityResource isAdmin: isAdmin autenticado.");
+        return ResponseEntity.ok().build();
+    }
+
+}
